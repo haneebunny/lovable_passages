7c238a66f5fbb07d08ec9dc123eed664d8c54e1f
PADUCK <re____@naver.com>
2023-08-01 17:07:48 +0900

    🫥 temp

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,29 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import KakaoProvider from "next-auth/providers/kakao";
+
+const handler = NextAuth({
+  providers: [
+    KakaoProvider({
+      clientId: process.env.KAKAO_ID,
+      clientSecret: process.env.KAKAO_SECRET,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token }) {
+      token.userRole = "admin";
+      return token;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
---
+++ src/components/common/user/index.tsx
@@ -0,0 +1,15 @@
+// "use client";
+
+// import { SessionProvider, useSession } from "next-auth/react";
+
+// function Wrapping() {
+
+//   return <SessionProvider>{children}</SessionProvider>;
+
+// }
+
+// export default function UserSetting({ children }) {
+//   const { data } = useSession();
+
+//   return <SessionProvider>{children}</SessionProvider>;
+// }
