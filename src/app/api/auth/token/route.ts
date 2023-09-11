b6d033b7649e13167bc123eed1c83c5d459f7f31
haneebunny <re____@naver.com>
2023-09-11 12:33:23 +0900

    🥹next-auth token을 어쩌지

--- src/app/api/auth/token/route.ts
+++ src/app/api/auth/token/route.ts
@@ -1,20 +1,24 @@
-import { NextResponse } from "next/server";
-import { NextApiRequest, NextApiResponse } from "next";
-import { getToken } from "next-auth/jwt";
+// Type error: Route "src/app/api/auth/token/route.ts" has an invalid "GET" export:
+//   Type "NextApiRequest" is not a valid type for the function's first argument.
+//     Expected "NextRequest | Request", got "NextApiRequest".
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    if (!req)
-      return NextResponse.json({
-        text: "로그인 요청을 다시 한 번 확인해주세요.",
-      });
-    const token = await getToken({ req });
-    if (token) {
-      return NextResponse.json(token);
-    } else {
-      return NextResponse.json({ text: "로그인 상태가 아닙니다." });
-    }
-  } catch (error) {
-    return NextResponse.error();
-  }
-}
+// import { NextResponse } from "next/server";
+// import { NextApiRequest, NextApiResponse } from "next";
+// import { getToken } from "next-auth/jwt";
+
+// export async function GET(req: NextApiRequest, res: NextApiResponse) {
+//   try {
+//     if (!req)
+//       return NextResponse.json({
+//         text: "로그인 요청을 다시 한 번 확인해주세요.",
+//       });
+//     const token = await getToken({ req });
+//     if (token) {
+//       return NextResponse.json(token);
+//     } else {
+//       return NextResponse.json({ text: "로그인 상태가 아닙니다." });
+//     }
+//   } catch (error) {
+//     return NextResponse.error();
+//   }
+// }
