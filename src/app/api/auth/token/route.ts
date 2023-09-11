aee56030a8cf7d901832f39203dd23bd67b6a465
haneebunny <re____@naver.com>
2023-09-11 16:44:50 +0900

    🐛 try catch 삭제

--- src/app/api/auth/token/route.ts
+++ src/app/api/auth/token/route.ts
@@ -6,18 +6,16 @@ import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function GET(req: NextRequest) {
-  try {
-    if (!req)
-      return NextResponse.json({
-        text: "로그인 요청을 다시 한 번 확인해주세요.",
-      });
-    const token = await getToken({ req });
-    if (token) {
-      return NextResponse.json(token);
-    } else {
-      return NextResponse.json({ text: "로그인 상태가 아닙니다." });
-    }
-  } catch (error) {
-    return NextResponse.error();
+  if (!req)
+    return NextResponse.json({
+      text: "로그인 요청을 다시 한 번 확인해주세요.",
+    });
+  const token = await getToken({ req });
+  if (token) {
+    return NextResponse.json(token);
+  } else {
+    return NextResponse.json({ text: "로그인 상태가 아닙니다." });
   }
+
+  return NextResponse.error();
 }
