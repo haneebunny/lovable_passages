5f5cd4982a941526fcdd0a750ca8057bf0f84571
haneebunny <re____@naver.com>
2023-09-11 16:40:40 +0900

    🐛 NextRequest

--- src/app/api/auth/token/route.ts
+++ src/app/api/auth/token/route.ts
@@ -2,23 +2,22 @@
 //   Type "NextApiRequest" is not a valid type for the function's first argument.
 //     Expected "NextRequest | Request", got "NextApiRequest".
 
-// import { NextResponse } from "next/server";
-// import { NextApiRequest, NextApiResponse } from "next";
-// import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
 
-// export async function GET(req: NextApiRequest, res: NextApiResponse) {
-//   try {
-//     if (!req)
-//       return NextResponse.json({
-//         text: "로그인 요청을 다시 한 번 확인해주세요.",
-//       });
-//     const token = await getToken({ req });
-//     if (token) {
-//       return NextResponse.json(token);
-//     } else {
-//       return NextResponse.json({ text: "로그인 상태가 아닙니다." });
-//     }
-//   } catch (error) {
-//     return NextResponse.error();
-//   }
-// }
+export async function GET(req: NextRequest) {
+  try {
+    if (!req)
+      return NextResponse.json({
+        text: "로그인 요청을 다시 한 번 확인해주세요.",
+      });
+    const token = await getToken({ req });
+    if (token) {
+      return NextResponse.json(token);
+    } else {
+      return NextResponse.json({ text: "로그인 상태가 아닙니다." });
+    }
+  } catch (error) {
+    return NextResponse.error();
+  }
+}
