1577df333c3737f545c7cae58fcaad5b9444858a
haneebunny <re____@naver.com>
2023-09-11 11:59:55 +0900

    🐛request type error

--- src/app/api/auth/token/route.ts
+++ src/app/api/auth/token/route.ts
@@ -4,6 +4,10 @@ import { getToken } from "next-auth/jwt";
 
 export async function GET(req: NextApiRequest, res: NextApiResponse) {
   try {
+    if (!req)
+      return NextResponse.json({
+        text: "로그인 요청을 다시 한 번 확인해주세요.",
+      });
     const token = await getToken({ req });
     if (token) {
       return NextResponse.json(token);
